d4150ea1b2312969bc5294497b2be39ba944d3c4
bavajitu <bavajithuarchlinux@gmail.com>
2025-02-17 21:03:24 +0530

    Second commit: calculator app

--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
-#include <iostream> 
-#include <array>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main () {
-    std::array<int, 3> my_array = {3, 4, 5};
-    std::cout << back.my_array << std::endl;
+    char op;
+    double a, b, result;
+
+    std::cout << "What is the operation you want to use?: ";    
+    cin >> op;
+
+    std::cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    switch(op) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+        default:
+            std::cout << "ERROR! The operator used isn't correct. ";
+            result = -DBL_MAX;
+
+    }
+    if(result != -DBL_MAX) {
+        std::cout << "Result: " << result << std::endl;
+    }
     return 0;
 }
